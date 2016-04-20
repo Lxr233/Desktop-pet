58c9fe12ccc95710d680d74ec00fb779ddfea595
Lxr233 <nnlxr11@163.com>
2016-04-20 15:26:12 +0800

    重做了属性设置界面
    重做界面，实现了相应的功能

--- Pet/app/src/main/java/com/example/administrator/pet/FirstFragment.java
+++ Pet/app/src/main/java/com/example/administrator/pet/FirstFragment.java
@@ -1,7 +1,7 @@
 package com.example.administrator.pet;
 
 /**
- * Created by wujia on 2016/4/4.
+ * Created by Lxr on 2016/4/19.
  */
 import android.content.Context;
 import android.content.Intent;
@@ -23,7 +23,6 @@ import com.example.administrator.pet.view.PetSelect;
 public class FirstFragment extends Fragment {
     ToolbarView first_toolbarview;
     private PetSelect first,second,third,fourth;
-    private Switch pet_switch1,pet_switch2,pet_switch3,pet_switch4;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
@@ -34,72 +33,22 @@ public class FirstFragment extends Fragment {
         second = (PetSelect)rootView.findViewById(R.id.select_2);
         third = (PetSelect)rootView.findViewById(R.id.select_3);
         fourth = (PetSelect)rootView.findViewById(R.id.select_4);
-
         init();
         setListener();
 
-//        unlock = (RelativeLayout)rootView.findViewById(R.id.qiaoba);
-//        first_toolbarview.setToolbar_text("形象选择");
-//        first_toolbarview.settoolbar_more_Visibility(View.GONE);
-//        first_toolbarview.settoolbar_relative_Visibility(View.GONE);
-//        fragment_first__more1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                System.out.println("wujiawen+++S1");
-//                Intent intent = new Intent();
-//                Bundle bundle = new Bundle();
-//                bundle.putString("flag", "1");
-//                intent.putExtras(bundle);
-//                intent.setClass(getActivity(), SettingActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        fragment_first__more2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent=new Intent();
-//                Bundle bundle=new Bundle();
-//                bundle.putString("flag", "2");
-//                intent.putExtras(bundle);
-//                intent.setClass(getActivity(), SettingActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-
-//        pet_switch1=(Switch)rootView.findViewById(R.id.pet_switch1);
-//        pet_switch2=(Switch)rootView.findViewById(R.id.pet_switch2);
-//        pet_switch3=(Switch)rootView.findViewById(R.id.pet_switch3);
-//        pet_switch4=(Switch)rootView.findViewById(R.id.pet_switch4);
-//        pet_switch3.setClickable(false);
-//        pet_switch4.setClickable(false);
-
-//        int i = sharedPreferences.getInt("count",0);
-//        if(i == 1) {
-//            pet_switch2.setClickable(true);
-//            unlock.setBackgroundResource(R.drawable.qiao_unlock);
-//            pet_switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//                @Override
-//                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                    if (isChecked) {
-//                        pet_switch2.setChecked(false);
-//                    }
-//                }
-//            });
-//            pet_switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//                @Override
-//                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                    if (isChecked) {
-//                        pet_switch1.setChecked(false);
-//                    }
-//                }
-//            });
-//        }
-//        else {
-//            pet_switch2.setClickable(false);
-//        }
-
 
         return rootView;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        first.setName(sharedPreferences.getString("name1", "皮卡"));
+        first.setCheck(sharedPreferences.getBoolean("isFirstOn", false));
+        if(sharedPreferences.getBoolean("isSecondUnlock",false)){
+            second.setName(sharedPreferences.getString("name2", "鳄鱼"));
+            second.setCheck(sharedPreferences.getBoolean("isSecondOn",false));
+        }
     }
 
     private void init(){
@@ -108,15 +57,14 @@ public class FirstFragment extends Fragment {
         second.setenabled(false);
         third.setenabled(false);
         fourth.setenabled(false);
-        second.setPetImg(R.drawable.kong_unlock);
+        first.setPetImg(R.drawable.pika);
+        if(!sharedPreferences.getBoolean("isSecondUnlock",false))
+            second.setPetImg(R.drawable.kong_unlock);
+        else
+            second.setPetImg(R.drawable.kong);
         third.setPetImg(R.drawable.qiao_unlock);
         fourth.setPetImg(R.drawable.v_unlock);
-        first.setName(sharedPreferences.getString("name1", "皮卡"));
-        first.setCheck(sharedPreferences.getBoolean("isFirstOn",false));
-        if(sharedPreferences.getBoolean("isSecondUnlock",false)){
-            second.setName(sharedPreferences.getString("name2", "鳄鱼"));
-            second.setCheck(sharedPreferences.getBoolean("isSecondOn",false));
-        }
+
     }
 
     private void setListener(){
--- Pet/app/src/main/java/com/example/administrator/pet/SettingActivity.java
+++ Pet/app/src/main/java/com/example/administrator/pet/SettingActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import java.util.Calendar;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -16,69 +17,61 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.demo.floatwindowdemo.R;
 
 /**
- * Created by wujia on 2016/4/15.
+ * Created by Lxr on 2016/4/20.
  */
 public class SettingActivity extends Activity implements OnClickListener{
-    SharedPreferences sharedpreferences;    //读数据
+    SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
-    AlertDialog dialog_name,dialog_birthday,dialog_character;    //对话框
-    LinearLayout set_ll_imagelinearlayout,name_linearlayout,birthday_linearlayout,character_linearlayout;
-    ImageView set_iv_imageview;
-    EditText name_et_editname,character_et_editcharacter,set_et_showname,set_et_showbirthday,set_et_showcharacter;
+    ImageView setImg;
+    EditText set_et_name,set_et_birthday,set_et_character;
     //TextView set_et_showname,set_te_showbirthday,set_et_showcharacter;
-    Button titlebar_bt_return,titlebar_bt_save,set_bt_birthday;
+    Button titlebar_bt_return,set_bt_birthday;
+    TextView titlebar_bt_save;
     //set_bt_character,set_bt_name,name_bt_cannel,name_bt_enter,character_bt_enter,character_bt_cannel;
     String flag;//标志位
-    int height,width,pixelColor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.set);
-        //getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
-        set_ll_imagelinearlayout=(LinearLayout)findViewById(R.id.set_ll_imagelinearlayout);
-        set_iv_imageview=(ImageView)findViewById(R.id.set_iv_imageview);
-        set_et_showname=(EditText)findViewById(R.id.set_et_showname);
-        set_et_showbirthday=(EditText)findViewById(R.id.set_et_showbirthday);
-        set_et_showcharacter=(EditText)findViewById(R.id.set_et_showcharacter);
+
+        init();
+
+
+    }
+
+    private void init(){
+        setImg = (ImageView)findViewById(R.id.set_img);
+        set_et_name=(EditText)findViewById(R.id.set_et_name);
+        set_et_birthday=(EditText)findViewById(R.id.set_et_birthday);
+        set_et_character=(EditText)findViewById(R.id.set_et_character);
         titlebar_bt_return=(Button)findViewById(R.id.titlebar_bt_return);
-        titlebar_bt_save=(Button)findViewById(R.id.titlebar_bt_save);
-        //set_bt_name=(Button)findViewById(R.id.set_bt_name);
+        titlebar_bt_save=(TextView)findViewById(R.id.titlebar_bt_save);
         set_bt_birthday=(Button)findViewById(R.id.set_bt_birthday);
-        //set_bt_character=(Button)findViewById(R.id.set_bt_character);
+        sharedPreferences = getSharedPreferences("pet", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
         //为按钮绑定监听器
         titlebar_bt_return.setOnClickListener(this);
         titlebar_bt_save.setOnClickListener(this);
-        //set_bt_name.setOnClickListener(this);
         set_bt_birthday.setOnClickListener(this);
-        //set_bt_character.setOnClickListener(this);
-        //获取SharedPreferences和Editor读写对象
-        sharedpreferences=this.getPreferences(MODE_PRIVATE);
-        editor=sharedpreferences.edit();
+
         //解码Bundle
         Bundle bundle=this.getIntent().getExtras();
         flag=bundle.getString("flag");
-        //根据标志位获取图片来源以及是指image_linearlayout的背景色
+        //根据标志位获取图片来源
         if(flag.equals("1")){
-            set_iv_imageview.setImageResource(R.drawable.pika);
-            Bitmap src =  BitmapFactory.decodeResource(getResources(),R.drawable.pika);
-            height = src.getHeight();
-            width = src.getWidth();
-            pixelColor = src.getPixel(width /200, height /200);
-            set_ll_imagelinearlayout.setBackgroundColor(pixelColor);
+            setImg.setImageResource(R.drawable.pika_edit);
         }
-	  if(flag.equals("2")){
-            set_iv_imageview.setImageResource(R.drawable.qiao);
-	    	Bitmap src =  BitmapFactory.decodeResource(getResources(),R.drawable.qiao);
-	        height = src.getHeight();
-	        width = src.getWidth();
-	        pixelColor = src.getPixel(width/200, height/200);
-            set_ll_imagelinearlayout.setBackgroundColor(pixelColor);
+        if(flag.equals("2")){
+            setImg.setImageResource(R.drawable.kong_edit);
         }
+
     }
     @Override
     public void onResume(){
@@ -94,27 +87,12 @@ public class SettingActivity extends Activity implements OnClickListener{
                 SettingActivity.this.finish();
                 break;
             case R.id.titlebar_bt_save:
-                editor.putString("name"+flag,set_et_showname.getText().toString());
-                editor.putString("birthday"+flag,set_et_showbirthday.getText().toString());
-                editor.putString("character"+flag,set_et_showcharacter.getText().toString());
+                editor.putString("name"+flag,set_et_name.getText().toString());
+                editor.putString("birthday"+flag,set_et_birthday.getText().toString());
+                editor.putString("character"+flag,set_et_character.getText().toString());
                 editor.commit();
                 SettingActivity.this.finish();
                 break;
-		/*case R.id.set_bt_name:
-			LayoutInflater inflater=LayoutInflater.from(this);
-			name_linearlayout=(LinearLayout)inflater.inflate(R.layout.name, null);
-			dialog_name=new AlertDialog.Builder(this)
-			.setView(name_linearlayout)
-			.create();
-			dialog_name.show();
-			name_et_editname=(EditText)name_linearlayout.findViewById(R.id.name_et_editname);
-			name_et_editname.setText(sharedpreferences.getString("name"+flag, null));
-			name_bt_enter=(Button)name_linearlayout.findViewById(R.id.name_bt_enter);
-			name_bt_cannel=(Button)name_linearlayout.findViewById(R.id.name_bt_cannel);
-			name_bt_enter.setOnClickListener(this);
-			name_bt_cannel.setOnClickListener(this);
-			break;
-			*/
             case R.id.set_bt_birthday:
                 Calendar c=Calendar.getInstance();
                 new DatePickerDialog(SettingActivity.this,new DatePickerDialog.OnDateSetListener() {
@@ -122,7 +100,7 @@ public class SettingActivity extends Activity implements OnClickListener{
                     @Override
                     public void onDateSet(DatePicker arg0, int year, int month, int day) {
                         // TODO Auto-generated method stub
-                        set_et_showbirthday.setText(year+"/"+(month+1)+"/"+day);
+                        set_et_birthday.setText(year+"/"+(month+1)+"/"+day);
 
                     }
                     //设置初始日期
@@ -131,49 +109,15 @@ public class SettingActivity extends Activity implements OnClickListener{
                         ,c.get(Calendar.MONTH)
                         ,c.get(Calendar.DAY_OF_MONTH)).show();
                 break;
-		/*case R.id.set_bt_character:
-			LayoutInflater inflater1=LayoutInflater.from(this);
-			character_linearlayout=(LinearLayout)inflater1.inflate(R.layout.character, null);
-			dialog_character=new AlertDialog.Builder(this)
-			.setView(character_linearlayout)
-			.create();
-			dialog_character.show();
-			character_et_editcharacter=(EditText)character_linearlayout.findViewById(R.id.character_et_editcharacter);
-			character_et_editcharacter.setText(sharedpreferences.getString("character"+flag, null));
-			character_bt_enter=(Button)character_linearlayout.findViewById(R.id.character_bt_enter);
-			character_bt_cannel=(Button)character_linearlayout.findViewById(R.id.character_bt_cannel);
-			character_bt_enter.setOnClickListener(this);
-			character_bt_cannel.setOnClickListener(this);
-			break;
-			*/
-	/*	case R.id.character_bt_enter:
-			editor.putString("character"+flag,character_et_editcharacter.getText().toString());
-			editor.commit();
-			set_tv_showcharacter.setText(character_et_editcharacter.getText().toString());
-			dialog_character.dismiss();
-			break;
-		case R.id.character_bt_cannel:
-			dialog_character.dismiss();
-			break;
-		case R.id.name_bt_enter:
-			editor.putString("name"+flag, name_et_editname.getText().toString());
-			editor.commit();
-			set_tv_showname.setText(name_et_editname.getText().toString());
-			dialog_name.dismiss();
-			break;
-		case R.id.name_bt_cannel:
-			dialog_name.dismiss();
-			break;
-			*/
             default:break;
         }
     }
     public void readFromSharedPreferences(String i){
-        String name=sharedpreferences.getString("name"+i, null);
-        String birthday=sharedpreferences.getString("birthday"+i, null);
-        String character=sharedpreferences.getString("character"+i, null);
-        set_et_showname.setText(name);
-        set_et_showbirthday.setText(birthday);
-        set_et_showcharacter.setText(character);
+        String name=sharedPreferences.getString("name"+i, null);
+        String birthday=sharedPreferences.getString("birthday"+i, null);
+        String character=sharedPreferences.getString("character"+i, null);
+        set_et_name.setText(name);
+        set_et_birthday.setText(birthday);
+        set_et_character.setText(character);
     }
 }
